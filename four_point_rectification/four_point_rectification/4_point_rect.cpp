82b198187d4f062060f233ab7071e89abf0c001e
Junqi Yang <junqiyang@tamu.edu>
2017-02-06 17:56:02 -0600

    use prespective wrapper

--- four_point_rectification/four_point_rectification/4_point_rect.cpp
+++ four_point_rectification/four_point_rectification/4_point_rect.cpp
@@ -10,13 +10,13 @@ void input_coordinate(Point2f opp[4], Point2f npp[4]);
 void find_matrix_h(CvMat *H, Point2f opp[4], Point2f npp[4]);
 void fill_matrix(CvMat *T, CvMat *old, Point2f opp[4], Point2f npp[4]);
 //step took to project image;
-void project_image(IplImage *input, CvMat *H);
+void project_image(Mat input, CvMat *H);
 int main(int argc, char** argv) {
 	char* filename_input;
 	char* filename_output;
 
-	IplImage* image_input;
-	IplImage* image_output;
+	Mat image_input;
+	Mat image_output;
 
 	Point2f old_pixel_point[4];
 	Point2f new_pixel_point[4];
@@ -30,11 +30,9 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-	if ((image_input = cvLoadImage(filename_input, 1)) == 0) {
-		printf("fail to load the image");
-		return 2;	
-	}
-	
+	image_input = imread(filename_input, 1);
+	imshow("testwindow", image_input);
+	cv::waitKey(0);
 	input_coordinate(old_pixel_point, new_pixel_point);
 	find_matrix_h(H_matrix, old_pixel_point, new_pixel_point);
 	visual_matrix(H_matrix, 3, 3);
@@ -43,16 +41,15 @@ int main(int argc, char** argv) {
 }
 
 
-void project_image(IplImage *input, CvMat *H){
+void project_image(Mat input, CvMat *H){
 	CvMat *Bound_input = cvCreateMat(3, 1, CV_64FC1);
 	CvMat *Bound_output = cvCreateMat(3, 1, CV_64FC1);
-	double Eli_x = cvmGet(Bound_output, 0, i) / x_3;
-	double Eli_y = cvmGet(Bound_output, 1, i) / x_3;
+
 
 	Point2f new_4_bound[4];
 
 	
-	//CvMat in_test = cvarrToMat(input, true);
+
 
 	cvmSet(Bound_input, 0, 0, 0);
 	cvmSet(Bound_input, 1, 0, 0);
@@ -61,48 +58,44 @@ void project_image(IplImage *input, CvMat *H){
 	new_4_bound[0].y = cvmGet(Bound_output, 1, 0) / cvmGet(Bound_output, 2, 0);
 
 
-	cvmSet(Bound_input, 0, 1, input->width -1);
-	cvmSet(Bound_input, 1, 1, 0);
-	cvmSet(Bound_input, 2, 1, 1);
+	cvmSet(Bound_input, 0, 0, input.size().width-1);
+	cvmSet(Bound_input, 1, 0, 0);
+	cvmSet(Bound_input, 2, 0, 1);
 	cvMatMul(H, Bound_input, Bound_output);	new_4_bound[1].x = cvmGet(Bound_output, 0, 0) / cvmGet(Bound_output, 2, 0);
 	new_4_bound[1].y = cvmGet(Bound_output, 1, 0) / cvmGet(Bound_output, 2, 0);
 
-	cvmSet(Bound_input, 0, 2, input->width - 1);
-	cvmSet(Bound_input, 1, 2, input->height - 1);
-	cvmSet(Bound_input, 2, 2, 1);
+	cvmSet(Bound_input, 0, 0, input.size().width-1);
+	cvmSet(Bound_input, 1, 0, input.size().height-1);
+	cvmSet(Bound_input, 2, 0, 1);
 	cvMatMul(H, Bound_input, Bound_output);	new_4_bound[2].x = cvmGet(Bound_output, 0, 0) / cvmGet(Bound_output, 2, 0);
 	new_4_bound[2].y = cvmGet(Bound_output, 1, 0) / cvmGet(Bound_output, 2, 0);
 
-	cvmSet(Bound_input, 0, 3, 0);
-	cvmSet(Bound_input, 1, 3, input->height - 1);
-	cvmSet(Bound_input, 2, 3, 1);
+	cvmSet(Bound_input, 0, 0, 0);
+	cvmSet(Bound_input, 1, 0, input.size().height - 1);
+	cvmSet(Bound_input, 2, 0, 1);
 	cvMatMul(H, Bound_input, Bound_output);	new_4_bound[3].x = cvmGet(Bound_output, 0, 0) / cvmGet(Bound_output, 2, 0);
 	new_4_bound[3].y = cvmGet(Bound_output, 1, 0) / cvmGet(Bound_output, 2, 0);
 
 	for (int i = 0; i < 4; i++) {
-	
-	
-	
+		printf("(x, y): %f, %f \n", new_4_bound[i].x, new_4_bound[i].y);	
 	}
-	/*double w_max = DBL_MIN;
+	double w_max = DBL_MIN;
 	double w_min = DBL_MAX;
 	double h_max = DBL_MIN;
 	double h_min = DBL_MAX;
 	for (int i = 0; i < 4; i++) {
-		double x_3 = cvmGet(Bound_output, 2, i);
-		double Eli_x = cvmGet(Bound_output, 0, i) / x_3;
-		double Eli_y = cvmGet(Bound_output, 1, i) / x_3;
-		if (Eli_x < w_min) {
-			w_min = Eli_x;
+
+		if (new_4_bound[i].x < w_min) {
+			w_min = new_4_bound[i].x;
 		}
-		if (Eli_y < h_min) {
-			h_min = Eli_y;
+		if (new_4_bound[i].y < h_min) {
+			h_min = new_4_bound[i].y;
 		}
-		if (Eli_x > w_max) {
-			w_max = Eli_x;
+		if (new_4_bound[i].x > w_max) {
+			w_max = new_4_bound[i].x;
 		}
-		if (Eli_y > h_max) {
-			h_max = Eli_y;
+		if (new_4_bound[i].y > h_max) {
+			h_max = new_4_bound[i].y;
 		}
 	}
 
@@ -110,6 +103,16 @@ void project_image(IplImage *input, CvMat *H){
 
 	double w = (w_max - w_min);
     double h = (h_max - h_min);
+	Mat out_test;
+	Mat H_mat = cvarrToMat(H);
+	Size image_size;
+	image_size.height = h;
+	image_size.width = w;
+	warpPerspective(input, out_test, H_mat , image_size);
+	imshow("window1",out_test);
+	cv::waitKey(0);
+	imshow("window2",input);
+	cv::waitKey(0);
 	/*IplImage *result = cvCreateImage(cvSize((input->height) * ratio, input->height), IPL_DEPTH_8U, 3);
 	cvZero(result);
 	CvMat out_test = cvarrToMat(result, true);
